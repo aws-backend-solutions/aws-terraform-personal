e61949aba83d589d491bf17da28d946c0603030e
sophia-bf <sophia.deleon@brightfin.com>
2024-05-16 17:16:05 +0800

    revert

--- us-dev/modules/alb/main.tf
+++ us-dev/modules/alb/main.tf
@@ -1,14 +1,18 @@
 resource "aws_lb" "aws_backend_load_balancer" {
-  name        = "aws-backend-nlb"
-  internal    = true
-  load_balancer_type = "network"
-  subnets     = [
+  name               = "aws-backend-lb"
+  internal           = true
+  security_groups    = [
+    var.aws_backend_security_group1_id
+    ]
+  subnets            = [
     var.aws_backend_private_subnet1_id,
-    var.aws_backend_private_subnet2_id
+    var.aws_backend_private_subnet2_id,
+    var.aws_backend_public_subnet1_id,
+    var.aws_backend_public_subnet2_id,
   ]
   tags = {
-    Name        = "aws-backend-nlb"
-    CostCenter  = var.cost_center_tag
+    Name       = "aws-backend-lb"
+    CostCenter = var.cost_center_tag
     Environment = var.environment_tag
   }
 }
@@ -16,7 +20,7 @@ resource "aws_lb" "aws_backend_load_balancer" {
 resource "aws_lb_listener" "aws_backend_load_balancer_listener" {
   load_balancer_arn = aws_lb.aws_backend_load_balancer.arn
   port              = 80
-  protocol          = "TCP"
+  protocol          = "HTTP"
 
   default_action {
     type             = "fixed-response"
