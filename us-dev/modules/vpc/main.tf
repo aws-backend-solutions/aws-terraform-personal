d1288a994e2ec84090c436b501f03d28c4afc263
sophia-bf <sophia.deleon@brightfin.com>
2024-05-17 16:09:11 +0800

    Update main.tf

--- us-dev/modules/vpc/main.tf
+++ us-dev/modules/vpc/main.tf
@@ -225,40 +225,21 @@ resource "aws_eip" "aws_backend_nat_eip1" {
   vpc = true
 }
 
-resource "aws_eip" "aws_backend_nat_eip2" {
-  vpc = true
-}
-
 # add NAT gateways to public subnets
 
-resource "aws_nat_gateway" "aws_backend_nat_gateway1" {
+resource "aws_nat_gateway" "aws_backend_nat_gateway" {
   allocation_id = aws_eip.aws_backend_nat_eip1.id
   subnet_id     = aws_subnet.aws_backend_public_subnet1.id
 
   tags = {
-    Name = "${var.prefix_name}-nat-gateway-1"
-  }
-}
-
-resource "aws_nat_gateway" "aws_backend_nat_gateway2" {
-  allocation_id = aws_eip.aws_backend_nat_eip2.id
-  subnet_id     = aws_subnet.aws_backend_public_subnet2.id
-
-  tags = {
-    Name = "${var.prefix_name}-nat-gateway-2"
+    Name = "${var.prefix_name}-nat-gateway"
   }
 }
 
 # add NAT gateways to private route table
 
-resource "aws_route" "aws_backend_ng_route1" {
+resource "aws_route" "aws_backend_ng_route" {
   route_table_id         = aws_route_table.aws_backend_private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.aws_backend_nat_gateway1.id
-}
-
-resource "aws_route" "aws_backend_ng_route2" {
-  route_table_id         = aws_route_table.aws_backend_private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.aws_backend_nat_gateway2.id
+  nat_gateway_id         = aws_nat_gateway.aws_backend_nat_gateway.id
 }
