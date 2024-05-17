984e2e251f4210eb702830268be8b2ff3c44a99e
sophia-bf <sophia.deleon@brightfin.com>
2024-05-17 22:55:43 +0800

    Update main.tf

--- us-dev/aws-mongodb-get-api/main.tf
+++ us-dev/aws-mongodb-get-api/main.tf
@@ -38,13 +38,14 @@ data "terraform_remote_state" "modules" {
 module "vpc" {
   source                             = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/aws-mongodb-get-api/vpc"
   prefix_name                        = var.prefix_name
+  aws_region                         = var.aws_region
   vpc_id_to_peer                     = var.vpc_id_to_peer
   cidr_block_of_vpc_to_peer          = var.cidr_block_of_vpc_to_peer
   aws_backend_vpc_id                 = data.terraform_remote_state.modules.outputs.aws_backend_vpc_id
   aws_backend_private_route_table_id = data.terraform_remote_state.modules.outputs.aws_backend_private_route_table_id
-  aws_backend_private_subnet1_id = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet1_id
-  aws_backend_private_subnet2_id = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet2_id
-  aws_backend_security_group3_id = data.terraform_remote_state.modules.outputs.aws_backend_security_group3_id
+  aws_backend_private_subnet1_id     = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet1_id
+  aws_backend_private_subnet2_id     = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet2_id
+  aws_backend_security_group3_id     = data.terraform_remote_state.modules.outputs.aws_backend_security_group3_id
 }
 
 module "lambda" {
