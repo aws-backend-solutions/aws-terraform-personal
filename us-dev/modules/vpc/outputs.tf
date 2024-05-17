b43ceea2f735590a43d795c14a2d9095d6370209
sophia-bf <sophia.deleon@brightfin.com>
2024-05-17 22:50:18 +0800

    Update outputs.tf

--- us-dev/modules/vpc/outputs.tf
+++ us-dev/modules/vpc/outputs.tf
@@ -33,16 +33,6 @@ output "aws_backend_security_group2_id" {
   value       = aws_security_group.aws_backend_security_group2.id
 }
 
-output "aws_backend_vpc_endpoint_id" {
-  description = "The ID of the AwsBackendVpcEndpoint."
-  value       = aws_vpc_endpoint.aws_backend_vpc_endpoint.id
-}
-
-output "aws_backend_vpc_endpoint_arn" {
-  description = "The ARN of the AwsBackendVpcEndpoint."
-  value       = aws_vpc_endpoint.aws_backend_vpc_endpoint.arn
-}
-
 output "aws_backend_private_route_table_id" {
   description = "The ID of private route table."
   value       = aws_route_table.aws_backend_private_route_table.id
