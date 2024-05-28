e34970b9dc30ea69fb6edb656c4c589cf6a11f66
sophia-bf <sophia.deleon@brightfin.com>
2024-05-28 19:36:31 +0800

    Update main.tf

--- us-dev/aws-customer-data-upload-api/api_gateway/main.tf
+++ us-dev/aws-customer-data-upload-api/api_gateway/main.tf
@@ -96,6 +96,18 @@ resource "aws_api_gateway_integration" "aws_customer_data_upload_api_integration
   integration_http_method = "PUT"
   uri                     = "arn:aws:apigateway:${var.aws_region}:s3:path/${var.aws_customer_data_upload_bucket_name}/*"
   credentials             = "${var.aws_backend_api_gateway_role_name}"
+
+  request_parameters = {
+    "integration.request.path.bucket" = "method.request.path.bucket"
+  }
+
+  request_templates = {
+    "application/json" = <<EOF
+    {
+      "bucket": "$input.params('bucket')"
+    }
+    EOF
+  }
 }
 
 # resource "aws_api_gateway_method" "aws_customer_data_upload_api_method1" {
