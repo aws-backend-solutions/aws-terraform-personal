c0c8852c21f9c45ea994df4a24211fa8c1ed479f
sophia-bf <sophia.deleon@brightfin.com>
2024-05-28 18:31:30 +0800

    Update main.tf

--- us-dev/modules/iam/main.tf
+++ us-dev/modules/iam/main.tf
@@ -15,3 +15,28 @@ resource "aws_iam_role" "aws_backend_api_gateway_role" {
 }
 EOF
 }
+
+resource "aws_iam_policy" "aws_backend_api_gateway_cloudwatch_logs_policy" {
+  name        = "${var.prefix_name}-api-gateway-cloudwatch-logs-policy"
+  description = "Allows API Gateway to push logs to CloudWatch Logs"
+  
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_attachment" {
+  role       = aws_iam_role.aws_backend_api_gateway_role.name
+  policy_arn = aws_iam_policy.aws_backend_api_gateway_cloudwatch_logs_policy.arn
+}
