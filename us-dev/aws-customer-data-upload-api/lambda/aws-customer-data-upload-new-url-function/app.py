b38ab70d820650777b59760e223f74171541c2d5
sophia-bf <sophia.deleon@brightfin.com>
2024-05-28 21:25:34 +0800

    update code

--- us-dev/aws-customer-data-upload-api/lambda/aws-customer-data-upload-new-url-function/app.py
+++ us-dev/aws-customer-data-upload-api/lambda/aws-customer-data-upload-new-url-function/app.py
@@ -1,31 +1,47 @@
 import json
 import boto3
-import logging
+import os
+import uuid
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    
-    file_path = 'path_to_your_file'
-    bucket_name = 'your_bucket_name'
-    object_key = 'desired_key_name_in_bucket'
+    print(event)
+    # Extracting bucket name and object key from the event
+    bucket_name = 'aws-customer-data-upload-bucket'
+    object_key = str(uuid.uuid4())  # Generate a unique object key
 
-    try:
-        s3.upload_file(file_path, bucket_name, object_key)
-        print(f"File uploaded successfully to bucket: {bucket_name} with key: {object_key}")
-        return {
-            'statusCode': 200,
-            'body': 'File uploaded successfully'
-        }
-    except Exception as e:
-        print(f"Error uploading file: {e}")
-        return {
-            'statusCode': 500,
-            'body': 'Error uploading file'
-        }
+    # Generate a pre-signed URL for uploading the file
+    presigned_url = s3_client.generate_presigned_url(
+        'put_object',
+        Params={'Bucket': bucket_name, 'Key': object_key},
+        ExpiresIn=3600  # URL expires in 1 hour
+    )
+    
+    # Extracting parameters from the event
+    file_content = 'text/plain'
+    content_type = 'text/plain'
+    
+    upload_to_s3_response = upload_to_s3(presigned_url, object_key, file_content, content_type)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'response': upload_to_s3_response, 'object_key': object_key})
+    }
+
+def upload_to_s3(presigned_url, object_key, file_content, content_type):
+    # Upload the file to S3 using the pre-signed URL
+    response = s3_client.put_object(
+        Bucket='aws-customer-data-upload-bucket',
+        Key=object_key,
+        Body=file_content,
+        ContentType=content_type
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'File uploaded successfully', 'response': response})
+    }
     
 def create_response(statusCode, body):
     response = {
