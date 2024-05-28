45eeaa937b9bb28f5950f7a2193ea4a5eecb1331
sophia-bf <sophia.deleon@brightfin.com>
2024-05-28 20:52:48 +0800

    add output

--- us-dev/modules/outputs.tf
+++ us-dev/modules/outputs.tf
@@ -68,3 +68,8 @@ output "aws_backend_api_gateway_role_name" {
   description = "The name of the aws_backend_api_gateway_role."
   value = module.iam.aws_backend_api_gateway_role_name
 }
+
+output "aws_backend_api_gateway_role_arn" {
+  description = "The ARN of the aws_backend_api_gateway_role."
+  value = module.iam.aws_backend_api_gateway_role.arn 
+}
