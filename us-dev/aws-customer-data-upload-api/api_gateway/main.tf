f350b27b22760bb30d60b2d8c1d53eb6b4d8d29c
sophia-bf <sophia.deleon@brightfin.com>
2024-05-28 19:18:13 +0800

    add put in api

--- us-dev/aws-customer-data-upload-api/api_gateway/main.tf
+++ us-dev/aws-customer-data-upload-api/api_gateway/main.tf
@@ -69,7 +69,7 @@ resource "aws_api_gateway_resource" "aws_customer_data_upload_api_resource" {
 resource "aws_api_gateway_method" "aws_customer_data_upload_api_method1" {
   rest_api_id   = aws_api_gateway_rest_api.aws_customer_data_upload_api.id
   resource_id   = aws_api_gateway_resource.aws_customer_data_upload_api_resource.id
-  http_method   = "POST"
+  http_method   = "PUT"
   authorization = "AWS_IAM"
 
   request_models = {
@@ -92,10 +92,41 @@ resource "aws_api_gateway_integration" "aws_customer_data_upload_api_integration
   http_method             = aws_api_gateway_method.aws_customer_data_upload_api_method1.http_method
   resource_id             = aws_api_gateway_resource.aws_customer_data_upload_api_resource.id
   rest_api_id             = aws_api_gateway_rest_api.aws_customer_data_upload_api.id
-  type                    = "AWS_PROXY"
-  integration_http_method = "POST"
-  uri                     = var.aws_customer_data_upload_function_invoke_arn1
+  type                    = "AWS"
+  integration_http_method = "PUT"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:s3:path/${var.aws_customer_data_upload_bucket_name}/*"
 }
+
+# resource "aws_api_gateway_method" "aws_customer_data_upload_api_method1" {
+#   rest_api_id   = aws_api_gateway_rest_api.aws_customer_data_upload_api.id
+#   resource_id   = aws_api_gateway_resource.aws_customer_data_upload_api_resource.id
+#   http_method   = "POST"
+#   authorization = "AWS_IAM"
+
+#   request_models = {
+#     "application/json" = "Error"
+#   }
+# }
+
+# resource "aws_api_gateway_method_settings" "aws_customer_data_upload_api_method_settings1" {
+#   rest_api_id = aws_api_gateway_rest_api.aws_customer_data_upload_api.id
+#   stage_name  = aws_api_gateway_stage.aws_customer_data_upload_api_stage.stage_name
+#   method_path = "*/*"
+
+#   settings {
+#     metrics_enabled = true
+#     logging_level   = "INFO"
+#   }
+# }
+
+# resource "aws_api_gateway_integration" "aws_customer_data_upload_api_integration1" {
+#   http_method             = aws_api_gateway_method.aws_customer_data_upload_api_method1.http_method
+#   resource_id             = aws_api_gateway_resource.aws_customer_data_upload_api_resource.id
+#   rest_api_id             = aws_api_gateway_rest_api.aws_customer_data_upload_api.id
+#   type                    = "AWS_PROXY"
+#   integration_http_method = "POST"
+#   uri                     = var.aws_customer_data_upload_function_invoke_arn1
+# }
 
 ##### method for the regenerate lambda #####
 
--- us-dev/aws-customer-data-upload-api/api_gateway/variables.tf
+++ us-dev/aws-customer-data-upload-api/api_gateway/variables.tf
@@ -2,6 +2,11 @@ variable "prefix_name" {
   type = string
 }
 
+variable "aws_region" {
+  type        = string
+  description = "Designated AWS_REGION where this solution will be deployed."
+}
+
 variable "cost_center_tag" {
   type        = string
   description = "Used for tagging the resources created."
@@ -41,4 +46,9 @@ variable "aws_customer_data_upload_function_invoke_arn1" {
 variable "aws_customer_data_upload_function_invoke_arn2" {
   type        = string
   description = "The Invoke ARN of the underlying regenerate new url Lambda function."
+}
+
+variable "aws_customer_data_upload_bucket_name" {
+  type = string
+  description = "The name of the designated s3 bucket for customers' data."
 }
