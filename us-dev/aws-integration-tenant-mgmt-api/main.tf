0b5e2e7adb088e1a9ebd09fe08b5bddfb3ae7bc0
sophia-bf <sophia.deleon@brightfin.com>
2024-06-07 17:59:19 +0800

    Update main.tf

--- us-dev/aws-integration-tenant-mgmt-api/main.tf
+++ us-dev/aws-integration-tenant-mgmt-api/main.tf
@@ -49,36 +49,37 @@ module "kms" {
 }
 
 module "lambda" {
-  source                                 = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/aws-integration-tenant-mgmt-api/lambda"
-  prefix_name                            = var.prefix_name
-  environment_tag                        = var.environment_tag
-  project_tag                            = var.project_tag
-  lambda_function_name                   = var.lambda_function_name
-  mongodb_domain                         = var.mongodb_domain
-  mongodb_url                            = var.mongodb_url
-  mongodb_name                           = var.mongodb_name
-  env_secret                             = var.env_secret
-  us_dev_domain                          = var.us_dev_domain
-  us_stage_domain                        = var.us_stage_domain
-  us_prod_domain                         = var.us_prod_domain
-  eu_stage_domain                        = var.eu_stage_domain
-  eu_prod_domain                         = var.eu_prod_domain
-  us_dev_usr                             = var.us_dev_usr
-  us_dev_pwd                             = var.us_dev_pwd
-  us_staging_usr                         = var.us_staging_usr
-  us_staging_pwd                         = var.us_staging_pwd
-  us_prod_usr                            = var.us_prod_usr
-  us_prod_pwd                            = var.us_prod_pwd
-  eu_staging_usr                         = var.eu_staging_usr
-  eu_staging_pwd                         = var.eu_staging_pwd
-  eu_prod_usr                            = var.eu_prod_usr
-  eu_prod_pwd                            = var.eu_prod_pwd
-  collection_name                        = var.collection_name
-  api_endpoint                           = var.api_endpoint
-  aws_backend_private_subnet1_id         = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet1_id
-  aws_backend_private_subnet2_id         = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet2_id
-  aws_backend_security_group2_id         = data.terraform_remote_state.modules.outputs.aws_backend_security_group2_id
-  aws_integration_tenant_mgmt_kms_key_id = module.kms.aws_integration_tenant_mgmt_kms_key_id
+  source                                     = "github.com/aws-backend-solutions/aws-terraform-personal/us-dev/aws-integration-tenant-mgmt-api/lambda"
+  prefix_name                                = var.prefix_name
+  environment_tag                            = var.environment_tag
+  project_tag                                = var.project_tag
+  lambda_function_name                       = var.lambda_function_name
+  mongodb_domain                             = var.mongodb_domain
+  mongodb_url                                = var.mongodb_url
+  mongodb_name                               = var.mongodb_name
+  env_secret                                 = var.env_secret
+  us_dev_domain                              = var.us_dev_domain
+  us_stage_domain                            = var.us_stage_domain
+  us_prod_domain                             = var.us_prod_domain
+  eu_stage_domain                            = var.eu_stage_domain
+  eu_prod_domain                             = var.eu_prod_domain
+  us_dev_usr                                 = var.us_dev_usr
+  us_dev_pwd                                 = var.us_dev_pwd
+  us_staging_usr                             = var.us_staging_usr
+  us_staging_pwd                             = var.us_staging_pwd
+  us_prod_usr                                = var.us_prod_usr
+  us_prod_pwd                                = var.us_prod_pwd
+  eu_staging_usr                             = var.eu_staging_usr
+  eu_staging_pwd                             = var.eu_staging_pwd
+  eu_prod_usr                                = var.eu_prod_usr
+  eu_prod_pwd                                = var.eu_prod_pwd
+  collection_name                            = var.collection_name
+  api_endpoint                               = var.api_endpoint
+  aws_backend_private_subnet1_id             = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet1_id
+  aws_backend_private_subnet2_id             = data.terraform_remote_state.modules.outputs.aws_backend_private_subnet2_id
+  aws_backend_security_group2_id             = data.terraform_remote_state.modules.outputs.aws_backend_security_group2_id
+  aws_integration_tenant_mgmt_kms_key_id     = module.kms.aws_integration_tenant_mgmt_kms_key_id
+  aws_integration_tenant_mgmt_kms_policy_arn = module.kms.aws_integration_tenant_mgmt_kms_policy_arn
 }
 
 module "api_gateway" {
