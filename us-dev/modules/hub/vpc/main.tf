b50a173cc41bff9aea5e87abe906383d8623b810
sophia-bf <sophia.deleon@brightfin.com>
2024-06-18 00:12:24 +0800

    Update main.tf

--- us-dev/modules/hub/vpc/main.tf
+++ us-dev/modules/hub/vpc/main.tf
@@ -203,23 +203,23 @@ resource "aws_vpc_endpoint" "primary_aws_backend_vpc_endpoint" {
 
 ##### nat gateway
 
-resource "aws_nat_gateway" "aws_backend_nat_gateway" {
-  allocation_id = aws_eip.aws_backend_nat_eip.id
-  subnet_id     = aws_subnet.aws_backend_public_subnet1.id
+resource "aws_nat_gateway" "primary_aws_backend_nat_gateway" {
+  allocation_id = aws_eip.primary_aws_backend_nat_eip.id
+  subnet_id     = aws_subnet.primary_aws_backend_public_subnet1.id
 
   tags = {
-    Name = "${var.prefix_name}-nat-gateway"
+    Name = "primary-${var.prefix_name}-nat-gateway"
   }
 }
 
-resource "aws_eip" "aws_backend_nat_eip" {
+resource "aws_eip" "primary_aws_backend_nat_eip" {
   vpc = true
 }
 
 # add nat gateways to private route table
 
-resource "aws_route" "aws_backend_ng_route" {
-  route_table_id         = aws_route_table.aws_backend_private_route_table.id
+resource "aws_route" "primary_aws_backend_ng_route" {
+  route_table_id         = aws_route_table.primary_aws_backend_private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.aws_backend_nat_gateway.id
+  nat_gateway_id         = aws_nat_gateway.primary_aws_backend_nat_gateway.id
 }
