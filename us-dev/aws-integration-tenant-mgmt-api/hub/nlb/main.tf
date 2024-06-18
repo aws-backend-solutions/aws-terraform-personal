ecbf32f0579acf1d3b125addfd7035d1e4a9d3b5
sophia-bf <sophia.deleon@brightfin.com>
2024-06-18 18:48:38 +0800

    update

--- us-dev/aws-integration-tenant-mgmt-api/hub/nlb/main.tf
+++ us-dev/aws-integration-tenant-mgmt-api/hub/nlb/main.tf
@@ -1,12 +1,12 @@
-resource "aws_lb" "primary_aws_backend_nlb" {
-  name               = "primary-${var.prefix_name}-nlb"
+resource "aws_lb" "primary_aws_integration_tenant_mgmt_nlb" {
+  name               = "${var.prefix_name}-nlb"
   internal           = false
   load_balancer_type = "network"
   security_groups    = [var.primary_aws_backend_security_group4_id]
   subnets            = var.primary_aws_backend_subnet_ids
 
   tags = {
-    Name        = "primary-${var.prefix_name}-nlb"
+    Name        = "${var.prefix_name}-nlb"
     CostCenter  = var.cost_center_tag
     Environment = var.environment_tag
   }
