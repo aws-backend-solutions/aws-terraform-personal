5ef3b8230463b44e5a4e53433578e89f6093feb6
sophia-bf <sophia.deleon@brightfin.com>
2024-06-18 21:30:46 +0800

    daw

--- us-dev/modules/hub/outputs.tf
+++ us-dev/modules/hub/outputs.tf
@@ -56,5 +56,6 @@ output "primary_aws_backend_vpc_endpoint_id" {
 }
 
 output "primary_aws_backend_vpc_endpoint_dns_name" {
-  value = aws_vpc_endpoint.primary_aws_backend_vpc_endpoint.dns_entry[0].dns_name
+  description = "The DNS of primary-aws-backend-api-vpce."
+  value = module.vpc.primary_aws_backend_vpc_endpoint.dns_entry[0].dns_name
 }
