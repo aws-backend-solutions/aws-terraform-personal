34750d5fa72ca7368087bbea49c8a226bca8f723
sophia-bf <sophia.deleon@brightfin.com>
2024-06-19 22:45:12 +0800

    Update outputs.tf

--- us-dev/modules/provider/nlb/outputs.tf
+++ us-dev/modules/provider/nlb/outputs.tf
@@ -1,9 +1,9 @@
-output "primary_aws_backend_nlb_dns_name" {
-  description = "DNS name of primary_aws_backend_nlb."
-  value       = aws_lb.primary_aws_backend_nlb.dns_name
+output "aws_backend_nlb_dns_name" {
+  description = "DNS name of aws_backend_nlb."
+  value       = aws_lb.aws_backend_nlb.dns_name
 }
 
-output "primary_aws_backend_nlb_arn" {
-  description = "ARN of primary_aws_backend_nlb."
-  value       = aws_lb.primary_aws_backend_nlb.arn
+output "aws_backend_nlb_arn" {
+  description = "ARN of aws_backend_nlb."
+  value       = aws_lb.aws_backend_nlb.arn
 }
