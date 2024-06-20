199b383b819125c7df1004f3394991c5f900aeb7
sophia-bf <sophia.deleon@brightfin.com>
2024-06-20 21:32:53 +0800

    Update main.tf

--- us-dev/modules/provider/main.tf
+++ us-dev/modules/provider/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "aws-backend-tf-state"
     key            = "modules/provider/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "${var.aws_region}"
     encrypt        = true
     dynamodb_table = "aws-backend-tf-lockid"
   }
