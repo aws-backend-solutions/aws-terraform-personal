bb20b1bc7b3d1957149ca9918c9d9d2e952f9de5
sophia-bf <sophia.deleon@brightfin.com>
2024-06-21 01:10:14 +0800

    Delete outputs.tf

--- us-dev/aws-integration-tenant-mgmt-api/consumer/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "aws_integration_tenant_mgmt_function_us_staging_invoke_arn" {
-  description = "The Invoke ARN of the underlying Lambda function."
-  value       = module.lambda.aws_integration_tenant_mgmt_function_us_staging_invoke_arn
-}
-
-output "aws_integration_tenant_mgmt_function_eu_staging_invoke_arn" {
-  description = "The Invoke ARN of the underlying Lambda function."
-  value       = module.lambda.aws_integration_tenant_mgmt_function_eu_staging_invoke_arn
-}
