7484a17fe21831437e588cc01a990a57455d88c6
sophia-bf <sophia.deleon@brightfin.com>
2024-06-20 13:49:08 +0800

    Update outputs.tf

--- us-dev/modules/consumer/outputs.tf
+++ us-dev/modules/consumer/outputs.tf
@@ -59,15 +59,15 @@ output "primary_aws_backend_vpc_endpoint_enis" {
 
 output "primary_aws_integration_tenant_mgmt_api_id" {
   description = "ID for primary_aws-integration-tenant-mgmt-api"
-  value       = module.api_gateway.primary_aws_integration_tenant_mgmt_api.id
+  value       = module.api_gateway.primary_aws_integration_tenant_mgmt_api_id
 }
 
 output "primary_aws_integration_tenant_mgmt_api_root_resource_id" {
   description = "Root resource ID for primary_aws-integration-tenant-mgmt-api"
-  value       = module.api_gateway.primary_aws_integration_tenant_mgmt_api.root_resource_id
+  value       = module.api_gateway.primary_aws_integration_tenant_mgmt_api_root_resource_id
 }
 
 output "primary_aws_integration_tenant_mgmt_api_body" {
   description = "Body of primary_aws-integration-tenant-mgmt-api"
-  value       = module.api_gateway.primary_aws_integration_tenant_mgmt_api.body
+  value       = module.api_gateway.primary_aws_integration_tenant_mgmt_api_body
 }
