a46ecdeba38ee777639b3f0b06fa8287d444d02b
sophia-bf <sophia.deleon@brightfin.com>
2024-06-26 04:41:42 +0800

    Update main.tf

--- us-dev/aws-integration-tenant-mgmt-api/consumer/lambda/main.tf
+++ us-dev/aws-integration-tenant-mgmt-api/consumer/lambda/main.tf
@@ -29,8 +29,37 @@ resource "aws_lambda_function" "aws_integration_tenant_mgmt_router_function" {
   timeout       = 60
   role          = aws_iam_role.aws_integration_tenant_mgmt_router_function_role.arn
   filename      = "${path.module}/code/${var.prefix_name}-router-function.zip"
+
+  environment {
+    variables = {
+      stage_name = var.stage_name
+    }
+  }
+
+  tags = {
+    CostCenter  = var.cost_center_tag
+    Environment = var.environment_tag
+    Project     = var.project_tag
+  }
+
+  vpc_config {
+    security_group_ids = [
+      var.primary_aws_backend_security_group2_id
+    ]
+    subnet_ids = [
+      var.primary_aws_backend_private_subnet1_id,
+      var.primary_aws_backend_private_subnet2_id
+    ]
+  }
 }
 
 resource "aws_cloudwatch_log_group" "aws_integration_tenant_mgmt_router_function_log_group" {
   name = "/aws/lambda/${aws_lambda_function.aws_integration_tenant_mgmt_router_function.function_name}"
+}
+
+resource "aws_lambda_permission" "aws_integration_tenant_mgmt_router_function_invoke_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.aws_integration_tenant_mgmt_router_function.arn
+  principal     = "apigateway.amazonaws.com"
 }
