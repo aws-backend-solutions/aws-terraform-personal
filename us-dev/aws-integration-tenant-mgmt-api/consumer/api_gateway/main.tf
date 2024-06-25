bea771e95c673e6441ffd22bba9e86719f86b8cd
sophia-bf <sophia.deleon@brightfin.com>
2024-06-26 03:06:34 +0800

    Update main.tf

--- us-dev/aws-integration-tenant-mgmt-api/consumer/api_gateway/main.tf
+++ us-dev/aws-integration-tenant-mgmt-api/consumer/api_gateway/main.tf
@@ -144,7 +144,7 @@ resource "aws_api_gateway_integration" "aws_integration_tenant_mgmt_api_tenants_
   rest_api_id             = var.primary_aws_integration_tenant_mgmt_api_id
   type                    = "AWS"
   integration_http_method = "POST"
-  uri                     = "${var.aws_integration_tenant_mgmt_sqs_queue_arn}/SendMessage"
+  uri                     = "${var.aws_integration_tenant_mgmt_sqs_queue_arn}:action/SendMessage"
 
   request_templates = {
     "application/json" = jsonencode({
